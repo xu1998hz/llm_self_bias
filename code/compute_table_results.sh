25f4aa06a5a3f750b8baa78446b8360c41ad702f
xu1998hz <wedxu@ucdavis.edu>
2024-02-02 11:48:19 +0000

    adding bleurt into pipeline

--- code/compute_table_results.sh
+++ code/compute_table_results.sh
@@ -1,10 +1,15 @@
-# first normalize all bleurt scores
-
 lang="yor-en"
-eval_name="gpt-4"
-model_name="gpt-4"
+eval_name="gpt-3.5-turbo"
+model_name="gpt-3.5-turbo"
+
 for iteration in "0" "1" "2" "3" "4" "5" "6" "7" "8" "9" "10"
 do 
+    # first compute all bleurt scores on translaiton outputs
+    CUDA_VISIBLE_DEVICES=0 python3 code/eval_bleurt.py -lang_dir yor-en \
+    -file_name "model_outputs/${model_name}/self_refine/${lang}/${model_name}-outputs/${lang}_self_refinement_100_${model_name}_new_${iteration}_rerun.txt" \
+    -save_name "model_outputs/${model_name}/self_refine/${lang}/bleurt-raw/${lang}_self_refinement_100_${eval_name}_new_${iteration}_rerun_bleurt.txt"
+
+    # first normalize all bleurt scores
     python3 code/quantile_mapping.py -human_file ../.mt-metrics-eval/mt-metrics-eval-v2/wmt22/human-scores/zh-en.mqm.seg.score \
     -obs_score_file ../.mt-metrics-eval/mt-metrics-eval-v2/wmt22/metric-scores/zh-en/BLEURT-20-refA.seg.score \
     -new_score_file "model_outputs/${model_name}/self_refine/${lang}/bleurt-raw/${lang}_self_refinement_100_${eval_name}_new_${iteration}_rerun_bleurt.txt" \
--- code/eval_bleurt.py
+++ code/eval_bleurt.py
@@ -4,20 +4,20 @@ import click
 
 @click.command()
 @click.option("-lang_dir", help="yor-en, en-de or zh-en")
-@click.option("-model_type")
-def main(model_type, lang_dir):
+@click.option("-file_name")
+@click.option("-save_name")
+def main(file_name, lang_dir, save_name):
     scorer = score.BleurtScorer("BLEURT-20")
     ref_lines = open(
         f"/mnt/data3/wendaxu/peril_self_improve/refs/{lang_dir}_ref_100.txt", "r"
     ).readlines()
     ref_lines = [ele[:-1] for ele in ref_lines]
 
-    cur_addr = f"model_outputs/{model_type}/{lang_dir}_base_outputs_{model_type}.txt"
-    out_lines = open(cur_addr, "r").readlines()
+    out_lines = open(file_name, "r").readlines()
     out_lines = [ele[:-1] for ele in out_lines]
     scores_ls = scorer.score(references=ref_lines, candidates=out_lines, batch_size=16)
     scores_ls = [str(score) + "\n" for score in scores_ls]
-    save_file = open(cur_addr.replace(".txt", "_bleurt.txt"), "w")
+    save_file = open(save_name, "w")
     save_file.writelines(scores_ls)
 
 
